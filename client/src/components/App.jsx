8c07802d66a989f7dc4a728223e3c1477c82c5ae
csherpa <chhoti0712@gmail.com>
2021-11-19 09:52:27 -0600

    basic setup done

---
+++ client/src/components/App.jsx
@@ -0,0 +1,21 @@
+// TODO
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Entuned</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
