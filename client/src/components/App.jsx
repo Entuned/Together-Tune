5b85ef535895814734d5feb45797d3753a8a3887
hnavar <83598123+hnavar@users.noreply.github.com>
2021-11-24 09:26:36 -0600

    stashing to work to work on design

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,51 +1,51 @@
-// // TODO
-// import React from 'react';
-// import axios from 'axios';
-// import ChatRoom from './ChatRoom.jsx';
 
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
+import React from 'react';
+import axios from 'axios';
+import ChatRoom from './ChatRoom.jsx';
 
-//     this.state = {
-//       messages: []
-//     };
-//     this.getMessages = this.getMessages.bind(this);
-//     this.postMessages = this.postMessages.bind(this);
-//   }
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-//   getMessages() {
-//     axios.get('http://localhost:3000/messages')
-//       .then((data) => {
-//         this.setState({
-//           messages: data.data
-//         });
-//       });
-//   }
+    this.state = {
+      messages: []
+    };
+    this.getMessages = this.getMessages.bind(this);
+    this.postMessages = this.postMessages.bind(this);
+  }
 
-//   postMessages(message) {
-//     console.log(message);
-//     const newMessage = {
-//       userName: 'testUser',
-//       message: message.text
-//     };
-//     console.log(newMessage);
-//     axios.post('http://localhost:3000/messages', newMessage)
-//       .then(() => this.getMessages())
-//       .catch(err => console.log(err));
-//   }
-//   componentDidMount() {
-//     this.getMessages();
-//   }
+  getMessages() {
+    axios.get('http://localhost:3000/messages')
+      .then((data) => {
+        this.setState({
+          messages: data.data
+        });
+      });
+  }
 
-//   render() {
-//     return (
-//       <div>
-//         <h1>Entuned-test-webpack</h1>
-//         <ChatRoom messages={this.state.messages} getMessages={this.getMessages} postMessages={this.postMessages}/>
-//       </div>
-//     );
-//   }
-// }
+  postMessages(message) {
+    console.log(message);
+    const newMessage = {
+      userName: 'testUser',
+      message: message.text
+    };
+    console.log(newMessage);
+    axios.post('http://localhost:3000/messages', newMessage)
+      .then(() => this.getMessages())
+      .catch(err => console.log(err));
+  }
+  componentDidMount() {
+    this.getMessages();
+  }
 
-// export default App;
+  render() {
+    return (
+      <div>
+        <h1>Entuned-test-webpack</h1>
+        <ChatRoom messages={this.state.messages} getMessages={this.getMessages} postMessages={this.postMessages}/>
+      </div>
+    );
+  }
+}
+
+export default App;
--- client/src/components/ChatRoomHooks.jsx
+++ client/src/components/ChatRoomHooks.jsx
@@ -1,47 +1,47 @@
-import SingleChat from './SingleChat.jsx';
-import React, { useState } from 'react';
+// import SingleChat from './SingleChat.jsx';
+// import React, { useState } from 'react';
 
-const ChatRoom = (props) => {
-  const [text, setText] = useState('');
+// const ChatRoom = (props) => {
+//   const [text, setText] = useState('');
  
-  const handleClick = (e) => {
-    e.preventDefault();
-    const {text} = this.state;
-    if (!text) { return; }
-    this.props.postMessages(this.state);
-    this.setState({
-      text: '',
-    });
-  };
+//   const handleClick = (e) => {
+//     e.preventDefault();
+//     const {text} = this.state;
+//     if (!text) { return; }
+//     this.props.postMessages(this.state);
+//     this.setState({
+//       text: '',
+//     });
+//   };
 
-  const componentDidMount = () => {
-    setInterval(() => {
-      this.props.getMessages();
-    }, 1000);
-  };
+//   const componentDidMount = () => {
+//     setInterval(() => {
+//       this.props.getMessages();
+//     }, 1000);
+//   };
 
-  return (
-    <div>
-      <div className='createMessage'>
-        <div className="formClass">New Message</div>
-        <form onSubmit={e => e.preventDefault()}>
-          <input
-            value={this.state.text}
-            onChange={(e) =>
-              this.setState({text: e.target.value })
-            }
-          />
-          <button onClick={(e) => this.handleClick(e)}>Submit Message</button>
-        </form>
-      </div>
+//   return (
+//     <div>
+//       <div className='createMessage'>
+//         <div className="formClass">New Message</div>
+//         <form onSubmit={e => e.preventDefault()}>
+//           <input
+//             value={this.state.text}
+//             onChange={(e) =>
+//               this.setState({text: e.target.value })
+//             }
+//           />
+//           <button onClick={(e) => this.handleClick(e)}>Submit Message</button>
+//         </form>
+//       </div>
 
-      <div className="singleChat">
-        {this.props.messages.map((message) => {
-          return <SingleChat key={message._id} message={message.message}/>;
-        })}
-      </div>
-    </div>
-  );
-};
+//       <div className="singleChat">
+//         {this.props.messages.map((message) => {
+//           return <SingleChat key={message._id} message={message.message}/>;
+//         })}
+//       </div>
+//     </div>
+//   );
+// };
 
-export default ChatRoom;
+// export default ChatRoom;
