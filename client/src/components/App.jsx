a5212f17a49d36af8406dafa310c67971cb1c00f
sbelete01 <sbelete01@gmail.com>
2021-11-24 13:21:44 -0600

    Fix: returning access token and added spotify api routes

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -6,61 +6,59 @@ import Login from './Login.jsx';
 // import sampleData from '/client/sampleData.js';
 import axios from 'axios';
 
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-//     this.state = {
-//       user: '',
-//       isLoggedIn: false
-//     };
-//     this.getUser = this.getUser.bind(this);
-//   }
+    this.state = {
+      accessTokenKey: ''
+    };
+  }
 
-//   getUser() {
-//     axios.get('/login')
-//       .then(({data}) => {
-//         console.log(data.user, 'DATA');
-//         this.setState({
-//           user: data.user
-//         });
-//       });
-//   }
-//   componentDidMount() {
-//     this.getUser();
-//   }
-//   render() {
-//     // const {user} = this.state;
-//     return (
-//       <div>
-//         <h1>Entuned-test-webpack</h1>
-//         <Login />
-//         {/* <p>{user.displayName}</p>
-//           <Playlists data={sampleData}/> */}
-//       </div>
-//     );
-//   }
-// }
 
-const App = () => {
-  useEffect(() => {
+  componentDidMount() {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const accessToken = urlParams.get('access_token');
     const refreshToken = urlParams.get('refresh_token');
+    this.setState({
+      accessTokenKey: accessToken
+    });
+    console.log(accessToken);
+  }
+  render() {
+    // const {user} = this.state;
+    return (
+      <div>
+        <h1>Entuned-test-webpack</h1>
+        <Login />
+        {this.state.accessTokenKey}
+        {/* <p>{user.displayName}</p>
+          <Playlists data={sampleData}/> */}
+      </div>
+    );
+  }
+}
 
-    console.log('accesstoken', accessToken);
-    console.log('refreshtoken', refreshToken);
-  }, []);
+// const App = () => {
+//   useEffect(() => {
+//     const queryString = window.location.search;
+//     const urlParams = new URLSearchParams(queryString);
+//     const accessToken = urlParams.get('access_token');
+//     const refreshToken = urlParams.get('refresh_token');
 
-  return (
-    <div>
-      <h1>Entuned-test-webpack</h1>
-      <Login />
-      {/* <p>{user.displayName}</p>
-      <Playlists data={sampleData}/> */}
-    </div>
-  );
-};
+//     console.log('accesstoken', accessToken);
+//     // console.log('refreshtoken', refreshToken);
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>Entuned-test-webpack</h1>
+//       <Login />
+//       {/* <p>{user.displayName}</p>
+//       <Playlists data={sampleData}/> */}
+//     </div>
+//   );
+// };
 
 export default App;
