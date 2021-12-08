5d2808f95ec62e67881304cea21ab1508dc51d77
zoobooks <zblaise23@gmail.com>
2021-12-07 19:05:29 -0600

    refactored playPlaylist and SinglePlaylist to functional components and deleted MusicPlayer.jsx

--- client/src/components/SinglePlayList.jsx
+++ client/src/components/SinglePlayList.jsx
@@ -1,26 +1,20 @@
 import React from 'react';
 
-class SinglePlayList extends React.Component {
-  constructor(props) {
-    super(props);
+
+const SinglePlayList = (props) => {
+
+  if (!!props.playlist.images.length) {
+    return <div>
+      <img style ={{width: '250px', height: '250px', border: '5px solid black' 
+      }}
+      onClick={() => props.handleClick(props.playlist)}
+      src={props.playlist.images[0].url}
+      alt="new"
+      />
+    </div>;
+  } else {
+    return <div/>;
   }
-
-  render() {
-    if (!!this.props.playlist.images.length) {
-      return <div>
-        <img style ={{width: '250px', height: '250px', border: '5px solid black' 
-        }}
-        onClick={() => this.props.handleClick(this.props.playlist)}
-        src={this.props.playlist.images[0].url}
-        alt="new"
-        />
-      </div>;
-    } else {
-      return <div/>;
-    }
-  } 
-
 }
-
 
 export default SinglePlayList;
