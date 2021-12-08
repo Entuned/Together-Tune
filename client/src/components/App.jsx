f237862c5faeaab388f68aef6c0a353283b20c80
hnavar <83598123+hnavar@users.noreply.github.com>
2021-12-07 20:35:20 -0600

    component refactored

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react';
 import ChatRoom from './ChatRoom.jsx';
 import {Grid} from '@material-ui/core';
@@ -6,7 +5,6 @@ import Login from './Login.jsx';
 import Logout from './Logout.jsx';
 import Account from './Account.jsx';
 import { Button } from '@material-ui/core';
-import AccountHooks from './AccountHooks.jsx';
 
 const App = () => {
   const [ accessTokenKey, setAccessTokenKey ] = useState('');
@@ -16,44 +14,6 @@ const App = () => {
       const cookieToken = document.cookie.split(';').find((elem) => elem.trim().startsWith('access_token')).split('=')[1];
       setAccessTokenKey(cookieToken);
     } catch (err) {}
-
-
-  }
-
-  render() {
-    const {accessTokenKey} = this.state;
-    return (
-      <div>
-        <Grid
-          container
-          spacing={0}
-          direction="column"
-          alignItems="center"
-          style={{ minHeight: '200vh', backgroundColor: '#f1f3f8' }}>
-          <h1>Listen to music and chat!</h1>
-          {accessTokenKey ? (
-            <div>
-              <Button variant="contained" color="secondary"><Logout/></Button>
-              <AccountHooks/>
-              <Grid
-                container
-                spacing={0}
-                direction="column"
-                alignItems="center"
-                style={{ minHeight: '200vh', backgroundColor: '#f1f3f8' }}>
-                <ChatRoom ID={this.state.ID} accessTokenKey={accessTokenKey}/>
-              </Grid>
-            </div>
-          ) : (
-            <Button variant="contained" color="secondary"> <Login/> </Button>
-          )}
-        </Grid>
-      </div>
-    );
-  }
-}
-
-
 
   }, []);
 
@@ -85,6 +45,6 @@ const App = () => {
       </Grid>
     </div>
   );
-
+};
 
 export default App;
